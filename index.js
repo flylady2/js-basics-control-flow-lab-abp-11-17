3f83046928fa538fddf17c75379d4430767fbec9
Leslie Stevens <flylady@mail.utexas.edu>
2017-11-14 21:15:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ function ternaryCheckCity(city) {
   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous':
+    'Thank you so much';
+    break;
+    case 'not as generous':
+    'Thank you';
+    break;
+    else {
+      return 'Bye.';
+    }
+    }
+  }
   
 }
