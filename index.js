8d2941e04b87c0ae1797eade7d48cfc18c0ec462
Leslie Stevens <flylady@mail.utexas.edu>
2017-11-14 20:51:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
     return 'This one is one me!';
   } else if (feet > 1999 && feet < 2500) {
-    return 'I will gladly take your 30 bucks';
+    return 'I will gladly take your 30 bucks.';
   } else {
     return 'No can do';
   }
