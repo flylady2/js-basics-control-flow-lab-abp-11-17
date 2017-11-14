2ef4863b2d6fe89bcdf8c4cc0831f45a60308ccc
Leslie Stevens <flylady@mail.utexas.edu>
2017-11-14 20:44:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Write your code in this file!
 scuberGreetingForFeet(feet) {
   if (feet <= 400) {
-    console.log('This one is one me!'):
+    return 'This one is one me!';
   } else if (feet > 2500) {
-    console.log('No can do'):
+    return 'No can do';
   } else {
-    console.log('I will gladly take your 30 bucks')
+    return 'I will gladly take your 30 bucks';
   }
   }
   }
